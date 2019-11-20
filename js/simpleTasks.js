38bbb50bc0bd343db6f829ae00e0210f7efcd44a
Sergey Pomortsev <zaooooza@gmail.com>
2019-11-21 00:26:21 +0300

    123

--- js/simpleTasks.js
+++ js/simpleTasks.js
@@ -55,7 +55,7 @@ console.log(null == 0); // false
 console.log(null >= 0); // true
 console.log(undefined > 0); // false
 console.log(undefined < 0); // false
-console.log(undefined == 0); // false
+console.log(undefined === 0); // false
 
 console.log("a" > "А"); // true
 console.log("ананас" > "яблоко"); // false
