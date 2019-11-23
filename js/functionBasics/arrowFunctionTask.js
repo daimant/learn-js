51722f66098724438d9932889dd8436c451a197f
Sergey Pomortsev <zaooooza@gmail.com>
2019-11-23 23:24:06 +0300

    solve task arrow function

--- js/functionBasics/arrowFunctionTask.js
+++ js/functionBasics/arrowFunctionTask.js
@@ -13,3 +13,13 @@
 //    function() { alert("Вы отменили выполнение."); }
 //  );
 
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
+);
