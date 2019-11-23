0e6eefc77fe930a88bdc13aea29ff514f406196e
Sergey Pomortsev <zaooooza@gmail.com>
2019-11-23 23:03:33 +0300

    rename function task and add new task

---
+++ js/functionBasics/simpleArrowFunctionTask.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//  Замените код Function Expression стрелочной функцией:
+//
+//    function ask(question, yes, no) {
+//      if (confirm(question)) yes()
+//      else no();
+//    }
+//
+//  ask(
+//    "Вы согласны?",
+//    function() { alert("Вы согласились."); },
+//    function() { alert("Вы отменили выполнение."); }
+//  );
+
