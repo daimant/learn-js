1cbe85aad9d642d4365eda1ed9099a24eb9afa71
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-07 01:59:35 +0500

    some change

---
+++ js/leetCodeTasks/palindrome-number.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  let reverseX = x.toString().split('').reverse().join('');
+  return +reverseX === x ? true : false;
+};
---
+++ js/leetCodeTasks/reverse-integer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(n) {
+
+  var reverseN = +String(Math.abs(n)).split('').reverse().join('');
+
+  if (reverseN > 0x7FFFFFFF) {
+    return 0;
+  }
+
+  return n < 0 ? -reverseN : reverseN;
+
+};
