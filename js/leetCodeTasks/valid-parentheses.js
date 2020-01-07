461858cb59790526e71d90f5577d3e6f06ee7a41
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-08 04:57:22 +0500

    some change

--- js/leetCodeTasks/valid-parentheses.js
+++ js/leetCodeTasks/valid-parentheses.js
@@ -5,11 +5,6 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  let arr = s.split('');
-
-  // if (s === '') return true;
-  // if (s.length % 2) return false;
-
   for (let i = 0; i < arr.length;) {
     if (s.indexOf('()') === -1 && s.indexOf('{}') === -1 && s.indexOf('[]') === -1) break;
     if (s.indexOf('()') !== -1) s = s.slice(0,s.indexOf('()')) + s.slice(s.indexOf('()')+2,s.length);
@@ -17,5 +12,5 @@ var isValid = function(s) {
     if (s.indexOf('[]') !== -1) s = s.slice(0,s.indexOf('[]')) + s.slice(s.indexOf('[]')+2,s.length);
 
   }
-  return (s.length === 0) ? true : false;
+  return (s.length === 0);
 };
