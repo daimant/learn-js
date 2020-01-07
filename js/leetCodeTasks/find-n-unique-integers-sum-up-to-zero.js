7a2c0df12d71323be2d7db9444d37c0aea3d27b1
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-08 04:37:52 +0500

    some change

--- js/leetCodeTasks/find-n-unique-integers-sum-up-to-zero.js
+++ js/leetCodeTasks/find-n-unique-integers-sum-up-to-zero.js
@@ -33,3 +33,14 @@ var sumZero = function(n) {
   }
   return uniqArr;
 };
+
+
+// а вот так надо было)
+//
+// let sumZero = (n, A = []) => {
+//   if (n % 2) A.push(0);
+//   for( let i = 1; i <= n/2; i++) {
+//     A.push(i,-i);
+//   }
+//   return A
+// }
