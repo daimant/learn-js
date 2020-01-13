8ea150d1b61a7979a7ec1fd6585fb48bdd75b279
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-13 10:43:30 +0300

    some changes

--- js/leetCodeTasks/best-time-to-buy-and-sell-stock.js
+++ js/leetCodeTasks/best-time-to-buy-and-sell-stock.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @param {number[]} prices
@@ -8,13 +8,13 @@ var maxProfit = function(prices) {
   let max = 0;
   let min = prices[0];
 
-  for(let i = 1; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     if (prices[i] < min) min = prices[i];
-    if (prices[i] - min > max ) max = prices[i] - min;
+    if (prices[i] - min > max) max = prices[i] - min;
   }
-  return max
-};
 
+  return max;
+};
 
 // очень долгий метод с сортировкой
 // /**
