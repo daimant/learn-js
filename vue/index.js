0e958048d6cf6812795b65632c859615c2913028
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-14 18:32:10 +0300

    some changes

---
+++ vue/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!"
+  },
+  methods: {
+    setMessage: function(event) {
+      this.message = event.target.value;
+    }
+  }
+});
