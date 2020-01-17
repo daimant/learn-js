2a099ef6f08bc5c07756da297d8ae8d892d5d793
Sergey Pomortsev <zaooooza@gmail.com>
2020-01-17 17:57:33 +0300

    some changes

---
+++ js/map-set/array-unique-map.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//Фильтрация уникальных элементов массива
+//Допустим, у нас есть массив arr.
+//  Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+//  Например:
+//function unique(arr) {
+//  /* ваш код */
+//}
+//
+//let values = ["Hare", "Krishna", "Hare", "Krishna",
+//  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+//];
+//
+//alert( unique(values) ); // Hare, Krishna, :-O
+//P.S. Здесь мы используем строки, но значения могут быть любого типа.
+//  P.P.S. Используйте Set для хранения уникальных значений.
+
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
+let values = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O"
+];
+
+console.log(unique(values)); // Hare, Krishna, :-O
+
+// неправильный метод
+//function unique(arr) {
+//  let set = new Set();
+//  let unique = [];
+//
+//  for(let i of arr) {
+//    set.add(i);
+//  }
+//
+//  for(let i of set.keys()) {
+//    unique.push(i)
+//  }
+//
+//  return unique;
+//}
