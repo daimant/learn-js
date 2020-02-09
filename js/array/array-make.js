36720126583be472a3804b8a6071f9f94d737cc6
Sergey Pomortsev <zaooooza@gmail.com>
2020-02-10 01:10:39 +0300

    some changes

--- js/array/array-make.js
+++ js/array/array-make.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let arr = [];
-for (let i = 0; i < 10000; i++) {
-  arr.push(Math.trunc(10 * Math.random()));
+for (let i = 0; i < 100; i++) {
+  arr.push([Math.ceil(1000 * Math.random()), Math.ceil(1000 * Math.random())]);
 }
-//arr.sort(() => 0.5 - Math.random());
+arr.sort(() => 0.5 - Math.random());
 //arr.sort((a, b) => (a > b ? 1 : -1));
 console.log(JSON.stringify(arr));
