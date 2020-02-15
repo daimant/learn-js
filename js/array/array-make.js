d8f1e8f06af8f08fb7b6da50481a31afb7c4eba5
Sergey Pomortsev <zaooooza@gmail.com>
2020-02-15 16:46:54 +0300

    add 409,412,414 leetcode problem

--- js/array/array-make.js
+++ js/array/array-make.js
@@ -1,8 +1,8 @@
 "use strict";
 
 let arr = [];
-for (let i = 0; i < 10000; i++) {
-  arr.push(Math.trunc(500 * Math.random()));
+for (let i = 0; i < 1000000; i++) {
+  arr.push(Math.trunc(100000 * Math.random()));
 }
 arr.sort(() => 0.5 - Math.random());
 //arr.sort((a, b) => (a > b ? 1 : -1));
